8e80de09baf2614a88a57289975e6e1392427ee4
Dickson Law <dickson.law@utoronto.ca>
2018-02-20 11:41:13 -0500

    Fix errors and silence debug messages

--- setup/boot.sh
+++ setup/boot.sh
@@ -4,11 +4,11 @@ export DEBIAN_FRONTEND=noninteractive
 echo "IIIF in a Box: Initializing setup..."
 
 # Add Mongo repo
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6 > /dev/null
-echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.4.list > /dev/null
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6 >/dev/null 2>&1
+echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" >/etc/apt/sources.list.d/mongodb-org-3.4.list
 
 # Update
-apt-get update -qqy > /dev/null
+apt-get update -qqy >/dev/null
 
 # Install basic dependencies
-apt-get -qqy install wget curl git unzip > /dev/null
+apt-get -qqy install wget curl git unzip >/dev/null
--- setup/ruby.sh
+++ setup/ruby.sh
@@ -4,8 +4,9 @@ export DEBIAN_FRONTEND=noninteractive
 echo "IIIF in a Box: Installing RVM and Ruby..."
 
 # Get RVM
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-curl -sSL https://get.rvm.io | bash -s stable
+gpg --quiet --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 >/dev/null 2>&1
+curl -sL https://get.rvm.io | bash -s stable --quiet-curl >/dev/null
 source /home/vagrant/.rvm/scripts/rvm || source /etc/profile.d/rvm.sh
-rvm use --default --install 2.4.1
-rvm cleanup all
+rvm install --quiet-curl 2.4.1 >/dev/null
+rvm use --default 2.4.1 >/dev/null
+rvm cleanup all >/dev/null
